33b4c337831f1e9b715f59497e7c5178493324e2
beeeg55 <beeeg@naver.com>
2020-04-19 01:55:19 +0900

    DP Practice

--- src/dp/dp_1463.java
+++ src/dp/dp_1463.java
@@ -3,22 +3,31 @@ package dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class dp_1932 {
-
+public class dp_1463 {
+	static int[] arr;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		
-		for(int i=0;i<n;i++){
-			String s = br.readLine();
-			StringTokenizer st = new StringTokenizer(s," ");
-			for(int j=0;j<i+1;j++){
-				
+		arr = new int[n+1];
+		arr[1]=0;
+		for(int i=2;i<n+1;i++){
+			int temp=arr[i-1]+1;
+			if(i%2==0){
+				if(temp>arr[i/2]+1){
+					temp = arr[i/2]+1;
+				}
 			}
+			if(i%3==0){
+				if(temp>arr[i/3]+1){
+					temp = arr[i/3]+1;
+				}
+			}
+			arr[i]=temp;
 		}
+		System.out.println(arr[n]);
+		
 	}
 
 }
